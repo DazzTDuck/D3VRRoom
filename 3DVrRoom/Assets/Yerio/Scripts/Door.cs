a8f3516c6d16630b3859380fff5d735753fcbf54
frzDazzT <58939437+frzDazzT@users.noreply.github.com>
2020-11-06 13:42:57 +0100

    open doors
    added lockable doors and being able to open them

---
+++ 3DVrRoom/Assets/Yerio/Scripts/Door.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (Rigidbody))]
+public class Door : MonoBehaviour
+{
+    public bool locked = true;
+    new Rigidbody rigidbody;
+
+    public Transform doorHandle;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.isKinematic = locked;
+    }
+
+   public void UnlockDoor()
+    {
+        if (locked)
+        {
+            locked = false;
+            rigidbody.isKinematic = locked;
+            Debug.Log("unlocked door");
+        }
+    }
+
+    public float GetDistance(Transform transform)
+    {
+        return Vector3.Distance(doorHandle.position, transform.position);
+    }
+}
---
+++ 3DVrRoom/Assets/Yerio/Scripts/Key.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public Door doorToOpen;
+    public float keyOpenDistance = 0.15f;
+    private void Update()
+    {
+        if (doorToOpen.GetDistance(gameObject.transform) <= keyOpenDistance)
+        {
+            if (doorToOpen.locked)
+                doorToOpen.UnlockDoor();
+        }
+    }
+}
