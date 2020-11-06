66730d6dc9db65aafd818ee9a2643f6fe48692e7
frzDazzT <58939437+frzDazzT@users.noreply.github.com>
2020-11-06 17:38:00 +0100

    updates
    -added keypad code
    -fixed door
    -fixed key

---
+++ 3DVrRoom/Assets/Yerio/Scripts/KeypadScreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class KeypadScreen : MonoBehaviour
+{
+    public TMP_Text keypadSceenText;
+
+    public void AddNumber(string number)
+    {
+        if(keypadSceenText.text.Length < 6)
+        {
+            if (keypadSceenText.text == "00000")
+                ClearText();
+
+            keypadSceenText.text += number;
+            Debug.Log(keypadSceenText.text);
+        }     
+    }
+
+    public void ClearText()
+    {
+        keypadSceenText.text = "";
+    }
+}
