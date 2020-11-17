e3dc225308ce6b3a1f5181b9853f9ca54c955837
frzDazzT <58939437+frzDazzT@users.noreply.github.com>
2020-11-17 15:06:22 +0100

    blacklight shader

---
+++ 3DVrRoom/Assets/Yerio/Shaders/BlackLightLamp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[ExecuteInEditMode]
+public class BlackLightLamp : MonoBehaviour
+{
+    public Material reveal;
+    public Light light;
+
+    void Update()
+    {
+        reveal.SetVector("Vector4_58A462A8", light.transform.position);
+        reveal.SetVector("Vector4_D5976873", -light.transform.forward);
+        reveal.SetFloat("Vector1_985FC512", light.spotAngle);
+    }
+}
