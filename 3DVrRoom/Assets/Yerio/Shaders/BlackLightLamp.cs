29bfa8f78e0334baf339e2979d3e4bd633d048c5
frzDazzT <58939437+frzDazzT@users.noreply.github.com>
2020-11-18 15:23:20 +0100

    tweaked UV light code

--- 3DVrRoom/Assets/Yerio/Shaders/BlackLightLamp.cs
+++ 3DVrRoom/Assets/Yerio/Shaders/BlackLightLamp.cs
@@ -2,14 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//[ExecuteInEditMode]
+[ExecuteInEditMode]
 public class BlackLightLamp : MonoBehaviour
 {
-    public Material reveal;
-    public Light light;
+    [SerializeField] Material reveal;
+    [SerializeField] Light light;
+
+    float minDistance = 0;
+    [SerializeField] float maxDistance;
+
+    float minStrength = 0;
+    [SerializeField] float maxStrength = 20;
+
+    float distance;
+    float strength;
+    float percentage;
 
     void Update()
     {
+        distance = Vector3.Distance(transform.position, light.gameObject.transform.position);
+        distance = Mathf.Clamp(distance, minDistance, maxDistance);
+
+        percentage = distance * 100 / maxDistance;
+        var newPercentage = 100 - percentage;
+
+        strength = (newPercentage / 100) * maxStrength;
+        //Debug.Log(strength);
+
+        reveal.SetFloat("Vector1_4E73A3A0", strength);
         reveal.SetVector("Vector4_58A462A8", light.transform.position);
         reveal.SetVector("Vector4_D5976873", -light.transform.forward);
         reveal.SetFloat("Vector1_985FC512", light.spotAngle);
