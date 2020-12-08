488dc4c96a1080b7f98a9d4bef615d57d21ec2b9
frzDazzT <58939437+frzDazzT@users.noreply.github.com>
2020-12-08 11:52:26 +0100

    updates

--- 3DVrRoom/Assets/Yerio/Scripts/Clock.cs
+++ 3DVrRoom/Assets/Yerio/Scripts/Clock.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+[ExecuteInEditMode]
 public class Clock : MonoBehaviour
 {
     [SerializeField] TMP_Text clockText;
---
+++ 3DVrRoom/Assets/Yerio/Scripts/Radio.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radio : MonoBehaviour
+{
+    [SerializeField] AudioClip[] sounds;
+
+    int soundsIndex = 0;
+    AudioSource source;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        source.clip = sounds[soundsIndex];
+    }
+
+    public void SwitchSounds()
+    {
+        soundsIndex++;
+
+        if(soundsIndex > sounds.Length - 1) { soundsIndex = 0; }
+
+        source.clip = sounds[soundsIndex];
+    }
+
+    public void TurnOn()
+    {
+        source.Play();
+    }
+    public void TurnOff()
+    {
+        source.Stop();
+    }
+}
