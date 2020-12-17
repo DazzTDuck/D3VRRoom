7016b47d0c5ef861d13afcdff3d7e5edcecbc506
frzDazzT <58939437+frzDazzT@users.noreply.github.com>
2020-12-17 14:26:58 +0100

    added checkmark animation

---
+++ 3DVrRoom/Assets/Yerio/Scripts/MenuCode/ToggleAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleAnimation : MonoBehaviour
+{
+    [SerializeField] Toggle toggle;
+    [SerializeField] Animator checkmarkAnimator;
+
+    public void Update()
+    {
+        if(toggle.isOn) { checkmarkAnimator.ResetTrigger("Off"); checkmarkAnimator.SetTrigger("On"); } 
+        else { checkmarkAnimator.ResetTrigger("On"); checkmarkAnimator.SetTrigger("Off"); }
+    }
+}
