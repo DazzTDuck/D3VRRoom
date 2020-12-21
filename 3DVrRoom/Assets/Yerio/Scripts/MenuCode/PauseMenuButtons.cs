a49c06fb1169ea28080c44b195ee0e5a86b1a513
frzDazzT <58939437+frzDazzT@users.noreply.github.com>
2020-12-21 14:00:43 +0100

    pause menu updated
    made it a small white board
    added settings
    added flip animations

--- 3DVrRoom/Assets/Yerio/Scripts/MenuCode/PauseMenuButtons.cs
+++ 3DVrRoom/Assets/Yerio/Scripts/MenuCode/PauseMenuButtons.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class PauseMenuButtons : MonoBehaviour
 {
     static PauseMenuManager pauseMenuManager;
+    SettingsMenu settings;
+    [SerializeField] Animator whiteBoardAnimator;
+
 
     private void Awake()
     {
         if(!pauseMenuManager)
         pauseMenuManager = FindObjectOfType<PauseMenuManager>();
+        settings = FindObjectOfType<SettingsMenu>();
     }
 
     public void ResumeGame()
@@ -24,10 +28,10 @@ public class PauseMenuButtons : MonoBehaviour
 
     public void OpenSettings()
     {
-
+        whiteBoardAnimator.SetTrigger("Flip");
     }
     public void CloseSettings()
     {
-
+        whiteBoardAnimator.SetTrigger("FlipBack");
     }
 }
